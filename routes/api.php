2dc8842f9187e679604b8ebd0689173ad86a3b5e
Matthew White <matt@matthewwhite.me.uk>
2026-02-18 21:55:47 +0000

    Actually, let's not name the routes. No need yet and it just adds clutter.

--- routes/api.php
+++ routes/api.php
@@ -9,41 +9,41 @@ use Spectacular\Core\Http\Controllers\Api\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(FeaturesController::class)->group(function () {
-    Route::post('features/{feature}/edit', 'update')->name('api.features.edit');
-    Route::post('features/add', 'store')->name('api.features.add');
-    Route::post('features/{feature}/delete', 'delete')->name('api.features.delete');
+    Route::post('features/{feature}/edit', 'update');
+    Route::post('features/add', 'store');
+    Route::post('features/{feature}/delete', 'delete');
 });
 
 Route::controller(ProjectsController::class)->group(function () {
-     Route::get('projects/browse', 'index')->name('api.projects.browse');
-    Route::post('projects/add', 'store')->name('api.projects.add');
-     Route::get('projects/{project}/read', 'show')->name('api.projects.read');
-    Route::post('projects/{project}/edit', 'update')->name('api.projects.edit');
-    Route::post('projects/{project}/delete', 'delete')->name('api.projects.delete');
-    Route::post('projects/{project}/organise', 'organise')->name('api.projects.organise');
+     Route::get('projects/browse', 'index');
+    Route::post('projects/add', 'store');
+     Route::get('projects/{project}/read', 'show');
+    Route::post('projects/{project}/edit', 'update');
+    Route::post('projects/{project}/delete', 'delete');
+    Route::post('projects/{project}/organise', 'organise');
 });
 
 Route::controller(UnknownsController::class)->group(function () {
-    Route::post('unknowns/{unknown}/edit', 'update')->name('api.unknowns.edit');
-    Route::post('unknowns/add', 'store')->name('api.unknowns.add');
-    Route::post('unknowns/{unknown}/delete', 'delete')->name('api.unknowns.delete');
+    Route::post('unknowns/{unknown}/edit', 'update');
+    Route::post('unknowns/add', 'store');
+    Route::post('unknowns/{unknown}/delete', 'delete');
 });
 
 Route::controller(RequirementsController::class)->group(function () {
-    Route::post('requirements/{requirement}/edit', 'update')->name('api.requirements.edit');
-    Route::post('requirements/add', 'store')->name('api.requirements.add');
-    Route::post('requirements/{requirement}/delete', 'delete')->name('api.requirements.delete');
-    Route::post('requirements/{requirement}/tasks/complete', 'completeTasks')->name('api.requirements.tasks.complete');
+    Route::post('requirements/{requirement}/edit', 'update');
+    Route::post('requirements/add', 'store');
+    Route::post('requirements/{requirement}/delete', 'delete');
+    Route::post('requirements/{requirement}/tasks/complete', 'completeTasks'));
 });
 
 Route::controller(TasksController::class)->group(function () {
-    Route::post('tasks/{task}/edit', 'update')->name('api.tasks.edit');
-    Route::post('tasks/add', 'store')->name('api.tasks.add');
-    Route::post('tasks/{task}/delete', 'delete')->name('api.tasks.delete');
+    Route::post('tasks/{task}/edit', 'update');
+    Route::post('tasks/add', 'store');
+    Route::post('tasks/{task}/delete', 'delete');
 });
 
 Route::controller(UsersController::class)->group(function () {
-    Route::post('users/add', 'store')->name('api.users.add');
-    Route::post('users/{user}/edit', 'update')->name('api.users.edit');
-    Route::post('users/{user}/delete', 'delete')->name('api.users.delete');
+    Route::post('users/add', 'store');
+    Route::post('users/{user}/edit', 'update');
+    Route::post('users/{user}/delete', 'delete');
 });
